95dde02dbb5a51b8624cdd6d58951b553b2f7831
YenaKC <yenakim242@icloud.com>
2026-06-11 23:28:38 +0200

    edit css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,31 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Navbar() {
-    return (
-        <nav>
-            <h2>Iron Mat Academy</h2>
+    const [isOpen, setIsOpen] = useState(false);
 
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-            <Link to="/coaches">Coaches</Link>
-            <Link to="/classes">Classes</Link>
-            <Link to="/techniques">Techniques</Link>
-            <Link to="/techniques/new">Add Technique</Link>
-            <Link to="/contact">Contact</Link>
+    return (
+        <nav className="navbar">
+            <div className="navbar-header">
+                <h2>Iron Mat Academy</h2>
+
+                <button
+                    className="menu-button"
+                    onClick={() => setIsOpen(!isOpen)}
+                >
+                    ☰
+                </button>
+            </div>
+
+            <div className={isOpen ? "nav-links open" : "nav-links"}>
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <Link to="/coaches">Coaches</Link>
+                <Link to="/classes">Classes</Link>
+                <Link to="/techniques">Techniques</Link>
+                <Link to="/techniques/new">Add Technique</Link>
+                <Link to="/contact">Contact</Link>
+            </div>
         </nav>
     );
 }
