cfa239255c7049dc0e8b864fdfb5f10a799361ab
YenaKC <yenakim242@icloud.com>
2026-06-11 19:06:56 +0200

    update About

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,5 +1,35 @@
 function AboutPage() {
-    return <h1>About Page</h1>;
+    return (
+        <main>
+            <section>
+                <h1>ABOUT US</h1>
+                <p>Forging character and technique through discipline.</p>
+            </section>
+
+            <section>
+                <h2>OUR MISSION</h2>
+                <p>
+                    At Iron Mat Academy, we believe that Jiu-Jitsu is more than a sport. It is a way to build discipline, confidence, and resilience through consistent training.
+                </p>
+                <p>
+                    Our mission is to create a respectful training environment where students can improve their technique, challenge themselves, and grow on and off the mat.
+                </p>
+            </section>
+
+            <section>
+                <h2>OUR FACILITIES</h2>
+
+                <div>
+                    <p>Premium Mats</p>
+                    <p>Climate Control</p>
+                    <p>Full Locker Room</p>
+                    <p>Shower Facilities</p>
+                    <p>Gear Storave</p>
+                    <p>Waiting Area</p>
+                </div>
+            </section>
+        </main>
+    );
 }
 
 export default AboutPage;
