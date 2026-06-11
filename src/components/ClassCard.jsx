990dff749fe4a57883556d95faafc5a063ca019d
YenaKC <yenakim242@icloud.com>
2026-06-12 01:55:11 +0200

    fix styling bug in clspg

--- src/components/ClassCard.jsx
+++ src/components/ClassCard.jsx
@@ -1,8 +1,8 @@
 function ClassCard({ classItem }) {
     return(
         <div className="class-card">
-            <h2>{classItem.title}</h2>
-            <p>{classItem.day}</p>
+            <p className="class-day">{classItem.day}</p>
+            <h3>{classItem.title}</h3>
             <p>{classItem.time}</p>
             <p>{classItem.level}</p>
             <p>Coach: {classItem.coach}</p>
