7e7a94d3c817f18a1d0044eb93fa0554e2b0f1e6
YenaKC <yenakim242@icloud.com>
2026-06-12 07:57:01 +0200

    filtered classes

--- src/pages/ClassesPage.jsx
+++ src/pages/ClassesPage.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+
 import ClassCard from "../components/ClassCard";
 
 function ClassesPage() {
     const [classes, setClasses] = useState([]);
+    const [selectedDay, setSelectedDay] = useState("All");
 
     useEffect(() => {
         axios
@@ -16,12 +18,31 @@ function ClassesPage() {
             });
     }, []);
 
+    const days = ["All", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    const filteredClasses =
+        selectedDay === "All"
+        ? classes
+        : classes.filter((classItem) => classItem.day === selectedDay);
+
     return (
         <main>
             <h1>Weekly Schedule</h1>
 
+            <section className="day-filter">
+                {days.map((day) => (
+                    <button
+                        key={day}
+                        onClick={() => setSelectedDay(day)}
+                        className={selectedDay === day ? "active-day" : ""}
+                    >
+                        {day}
+                    </button>
+                ))}
+            </section>
+
             <section className="classes-grid">
-                {classes.map((classItem) => (
+                {filteredClasses.map((classItem) => (
                     <ClassCard
                         key={classItem.id}
                         classItem={classItem}
