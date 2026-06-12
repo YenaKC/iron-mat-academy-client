046d8762bae44ad6dfb20e356972780bd4071a5c
YenaKC <yenakim242@icloud.com>
2026-06-12 07:30:20 +0200

    coach

--- src/pages/CoachesPage.jsx
+++ src/pages/CoachesPage.jsx
@@ -1,10 +1,63 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
+import alexPhoto from "../assets/coach-alex.png";
+import alexDrawing from "../assets/coach-alex-drawing.png";
+import mayaPhoto from "../assets/coach-maya.png";
+import mayaDrawing from "../assets/coach-maya-drawing.png";
+import leoPhoto from "../assets/coach-leo.png";
+import leoDrawing from "../assets/coach-leo-drawing.png";
+import ninaPhoto from "../assets/coach-nina.png";
+import ninaDrawing from "../assets/coach-nina-drawing.png";
+import minjiPhoto from "../assets/coach-minji.png";
+import minjiDrawing from "../assets/coach-minji-drawing.png";
+import harutoPhoto from "../assets/coach-haruto.png";
+import harutoDrawing from "../assets/coach-haruto-drawing.png";
+import rafaelPhoto from "../assets/coach-rafael.png";
+import rafaelDrawing from "../assets/coach-rafael-drawing.png";
+import raulPhoto from "../assets/coach-raul.png";
+import raulDrawing from "../assets/coach-raul-drawing.png";
+
 import CoachCard from "../components/CoachCard";
 
 function CoachesPage() {
     const [coaches, setCoaches] = useState([]);
+
+    // coach image mapping
+    const coachImage = {
+        "Coach Alex": {
+            photoImage: alexPhoto,
+            drawingImage: alexDrawing,
+        },
+        "Coach Maya": {
+            photoImage: mayaPhoto,
+            drawingImage: mayaDrawing,
+        },
+        "Coach Leo": {
+            photoImage: leoPhoto,
+            drawingImage: leoDrawing,
+        },
+        "Coach Nina": {
+            photoImage: ninaPhoto,
+            drawingImage: ninaDrawing,
+        },
+        "Coach Minji": {
+            photoImage: minjiPhoto,
+            drawingImage: minjiDrawing,
+        },
+        "Coach Haruto": {
+            photoImage: harutoPhoto,
+            drawingImage: harutoDrawing,
+        },
+        "Coach Rafael": {
+            photoImage: rafaelPhoto,
+            drawingImage: rafaelrawing,
+        },
+        "Coach Raul": {
+            photoImage: raulPhoto,
+            drawingImage: raulDrawing,
+        },
+    }
 
     useEffect(() => {
         axios
@@ -23,9 +76,12 @@ function CoachesPage() {
 
             <section className="coaches-grid">
                 {coaches.map((coach) => (
-                    <CoachCard 
+                    <CoachCard
                         key={coach.id}
-                        coach={coach}
+                        coach={{
+                            ...coach,
+                            ...coachImages[coach.name]
+                        }}
                     />
                 ))}
             </section>
