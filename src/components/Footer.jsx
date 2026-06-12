124746817864dffdfd6263e40419fb87f0253d5c
YenaKC <yenakim242@icloud.com>
2026-06-12 04:10:45 +0200

    error fix in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,33 +6,31 @@ function Footer() {
                 <div className="footer-brand">
                     <h3>IRON MAT ACADEMY</h3>
                     <p>Master your craft, dominate the mat</p>
+
+                    <div className="footer-socials">
+                        <img src="/tiktok.png" alt="TikTok" />
+                        <img src="/instagram.png" alt="Instagram" />
+                        <img src="/x.png" alt="X" />
+                        <img src="/facebook.png" alt="Facebook" />
+                    </div>
                 </div>
 
-                <div className="footer-socials">
-                    <img src="/tiktok.png" alt="TikTok" />
-                    <img src="/instagram.png" alt="Instagram" />
-                    <img src="/x.png" alt="X" />
-                    <img src="/facebook.png" alt="Facebook" />
+                <div className="footer-links">
+                    <h4>Explore</h4>
+                    <p>Home</p>
+                    <p>About</p>
+                    <p>Coaches</p>
+                    <p>Classes</p>
+                    <p>Techniques</p>
+                    <p>Contact</p>
                 </div>
-            </div>
 
-            <div className="footer-links">
-                <h4>Explore</h4>
-
-                <p>Home</p>
-                <p>About</p>
-                <p>Coaches</p>
-                <p>Classes</p>
-                <p>Techniques</p>
-                <p>Contact</p>
-            </div>
-
-            <div className="footer-contact">
-                <h4>Contact Us</h4>
-
-                <p>Carrer Guitard 48, Barcelona</p>
-                <p>info@ironmatacademy.com</p>
-                <p>Mon - Fri: 07:00 - 22:00 / Sat: 10:00 -13:00</p>
+                <div className="footer-contact">
+                    <h4>Contact Us</h4>
+                    <p>Carrer Guitard 48, Barcelona</p>
+                    <p>info@ironmatacademy.com</p>
+                    <p>Mon - Fri: 07:00 - 22:00 / Sat: 10:00 -13:00</p>
+                </div>
             </div>
 
             <div className="footer-bottom">
